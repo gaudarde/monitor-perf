bbe72c208910ff5145e4fc08cac617458d6f7ab2
gaudarde <Epzera1993>
2020-07-20 05:22:18 -0300

    dep

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import plotly.graph_objects as go
 
 # load stuffs
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 df = pd.read_csv('data/data.csv', encoding='cp1252', parse_dates=[
     'Início', 'Término', 'Conclusão'], dayfirst=True, index_col='Início')
 
