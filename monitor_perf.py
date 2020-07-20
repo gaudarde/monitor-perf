91a9febd070fedb7c802438772847a5502dfd392
gaudarde <Epzera1993>
2020-07-20 05:03:04 -0300

    app.py

--- monitor_perf.py
+++ monitor_perf.py
@@ -16,20 +16,13 @@ if __name__ == '__main__':
     pd.set_option('display.max_columns', 20)
     pd.set_option('display.max_rows', 500)
 
-    if datetime.fromtimestamp(os.path.getmtime('web/data/table.csv')).date() < datetime.today().date():
+    if datetime.fromtimestamp(os.path.getmtime('data/table.csv')).date() < datetime.today().date():
         download.download()
         download.merge()
     else:
         pass
 
     dados.dados()
-
-    pastas = ['web', 'web/data']
-    for i in pastas:
-        if not os.path.isdir(i):
-            os.mkdir(i)
-        else:
-            pass
 
     arquivo = max(glob.glob('arquivos_definitivos/*.csv'), key=os.path.getctime)
     df = pd.read_csv(arquivo, encoding='cp1252', parse_dates=[
@@ -70,13 +63,13 @@ if __name__ == '__main__':
 
     # Dados reorganizados, base para gerar os gráficos (data.csv)
     dfData = df.iloc[:, [7, 8, 9, 0, 1, 2, 17, 3, 4, 10, 5, 6, 14, 15, 11, 16, 12, 13]]
-    dfData.to_csv('web/data/data.csv', index=False, encoding='cp1252', decimal=',')
-    dfData.to_csv('web/assets/data.csv', index=False, encoding='cp1252', decimal=',')
+    dfData.to_csv('data/data.csv', index=False, encoding='cp1252', decimal=',')
+    dfData.to_csv('assets/data.csv', index=False, encoding='cp1252', decimal=',')
     print(f'df data.csv (todas as colunas)\n'
           f'{dfData.head()}\n')
 
     # Dados para a tabela (data.csv)
     dfTable = df.iloc[:, [7, 8, 0, 17, 3, 4, 10, 5, 15, 11, 16]]
-    dfTable.to_csv('web/data/table.csv', index=False, encoding='cp1252', decimal=',')
+    dfTable.to_csv('data/table.csv', index=False, encoding='cp1252', decimal=',')
     print(f'df table.csv (tabela)\n'
           f'{dfTable.head()}\n')
