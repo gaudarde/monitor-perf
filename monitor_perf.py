78e19b9abd67efcf045699b8742ab7956c209314
Gustavo Gaudarde <gustavogaudarde@gmail.com>
2020-12-24 12:04:59 -0300

    Atualização de dependências

--- monitor_perf.py
+++ monitor_perf.py
@@ -16,13 +16,17 @@ if __name__ == '__main__':
     pd.set_option('display.max_columns', 20)
     pd.set_option('display.max_rows', 500)
 
-    if datetime.fromtimestamp(os.path.getmtime('data/table.csv')).date() < datetime.today().date():
+    '''    if datetime.fromtimestamp(os.path.getatime('data/table.csv')).date() < datetime.today().date():
         download.download()
         download.merge()
     else:
-        pass
+        pass'''
 
-    dados.dados()
+    try:
+        download.download()
+    finally:
+        download.merge()
+        dados.dados()
 
     arquivo = max(glob.glob('arquivos_definitivos/*.csv'), key=os.path.getctime)
     df = pd.read_csv(arquivo, encoding='cp1252', parse_dates=[
